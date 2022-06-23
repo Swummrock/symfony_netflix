426fef1ddc88a37828122dc70ffec477821beac2
SE13codeur <se13codeur@gmail.com>
2022-06-23 11:01:21 +0200

    debugged some imports

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -7,7 +7,7 @@ use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class UserFixtures// extends Fixture
+class UserFixtures extends Fixture
 
 {
     public function __construct(private UserPasswordHasherInterface $passwordEncoder)
